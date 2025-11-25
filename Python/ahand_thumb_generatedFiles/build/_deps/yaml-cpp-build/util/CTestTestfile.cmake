2cddee57e53f67f3b30f69711fd8e444cb8dc4fb
LeonardoMarcello <leonardo.marcello_99@hotmail.com>
2025-11-25 18:09:53 +0100

    cleaning

---
+++ Python/ahand_thumb_generatedFiles/build/_deps/yaml-cpp-build/util/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/leo/Desktop/allegro_hand_ros2_ws/Python/ahand_thumb_generatedFiles/build/_deps/yaml-cpp-src/util
+# Build directory: /home/leo/Desktop/allegro_hand_ros2_ws/Python/ahand_thumb_generatedFiles/build/_deps/yaml-cpp-build/util
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
